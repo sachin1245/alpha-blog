07756a8cf517e7e7abdfe49d4e39c280cbe8459a
Rails First App on Git <sachin1245@hotmail.com>
2016-02-23 21:00:30 +0000

    copied fiels from the local system and update everything

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,40 +1,60 @@
 class ArticlesController < ApplicationController
-  def new
-    @article = Article.new
+  before_action :set_article, only: [:edit, :update, :show, :destoy]
+
+def index
+  @articles = Article.all
+end
+def new
+  @article = Article.new
+end
+
+def edit
+
+end
+
+
+def create
+  #render plain: params[:article].inspect
+  @article = Article.new(article_params)
+  if @article.save
+    flash[:notice] = "Article succesfully created"
+    redirect_to article_path(@article)
+  else
+    render 'new'
+  end
+end
+
+def update
+
+  if @article.update(article_params)
+    flash[:notice] = "Article succesfully updated"
+    redirect_to article_path(@article)
+  else
+    render 'edit'
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+end
 
-  def create
-    @article = Article.new(article_params)
-    @article.save
-    if @article.save
-      flash[:notice] = "Article was Succesfully Created"
-      redirect_to article_path(@article)
-    else
-      render 'new'
-    end
-  end
 
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
-      flash[:notice] = "Article Succesfully updated"
-      redirect_to article_path(@article)
-    else
-      render 'edit'
-    end
-  end
+def show
 
-  def show
-    @article = Article.find(params[:id])
-  end
+end
 
+def destroy
+  @article.destroy
+
+  flash[:notice] = "Article succesfully deleted"
+end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :description)
-    end
+
+  def set_article
+
+    @article = Article.find(params[:id])
+
+  end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
